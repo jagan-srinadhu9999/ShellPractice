db69c56e1a0cabecabd843db72d02959c52258a0
jagan <sansatya234@gmail.com>
2026-01-29 23:17:44 +0530

    nginx install

--- first-shell-file.sh
+++ first-shell-file.sh
@@ -3,6 +3,9 @@
 echo "Hello, World!"
 
 USER_ID=$(id -u)
+Current_User=$(whoami)
+Currebynt_Hostname=$(hostname)
+Current
 echo "Your user ID is: $USER_ID"
 
 if [ USER_ID -eq 0 ]; then
@@ -11,3 +14,12 @@ else
     echo "You are running this script as a regular user."
 fi
 
+Time = $(date)
+echo "Current date and time: $Time"
+
+if[$Current_User -ne 0]; then
+    echo "You are not root user, installing nginx..."
+    echo "check user name $Current_User"
+    sudo su -
+dnf install nginx -y
+fi
